001962ff302aff53d23194951213c94a5bbff8b6
xy <xxinyitoh@gmail.com>
2025-06-08 15:21:03 +0800

    fix: syntax
    removed spaces which breaks class, removed assignment for setIsDarkMode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -9,11 +9,11 @@ export const ThemeToggle = () => {
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
         if (storedTheme == "dark") {
-            setIsDarkMode = true;
+            setIsDarkMode(true);
             document.documentElement.classList.add("dark")
         } else {
             localStorage.setItem("theme", "light")
-            setIsDarkMode = false;
+            setIsDarkMode(false);
         }
     }, [])
 
@@ -36,8 +36,8 @@ export const ThemeToggle = () => {
         <button
             onClick={toggleTheme}
             className = {cn(
-                "fixed max-sm: hidden top-5 right-5 z-50 p-2 rounded-full transition-colors",
-                "duration-300 focus:outline-hidden"
+                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors",
+                "duration-300 focus:outline-none"
             )}
         > 
             {isDarkMode ? (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
 import {ThemeToggle} from "../components/ThemeToggle";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
 
-    { /* theme toggle (light/dark mode) */}
-    <ThemeToggle/>
+        { /* theme toggle (light/dark mode) */}
+        <ThemeToggle />
 
-    { /* bg effects */}
-
-
-    { /* nav bar */}
+        { /* bg effects */}
 
 
-    { /* main content */}
+        { /* nav bar */}
 
 
-    { /* footer */}
+        { /* main content */}
 
 
+        { /* footer */}
 
-    </div>;
-}
+    </div>
+    );
+};
