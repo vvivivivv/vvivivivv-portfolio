c902705c4041af9b25affb8b6d41427498063d22
xy <xxinyitoh@gmail.com>
2025-06-08 15:22:23 +0800

    fix: syntax
    removed space

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -41,7 +41,7 @@ export const ThemeToggle = () => {
             )}
         > 
             {isDarkMode ? (
-                < Sun className="h-6 w-6 text-yellow-300"/>
+                <Sun className="h-6 w-6 text-yellow-300"/>
             ) : (
                 <Moon className="h-6 w-6 text-blue-900"/>
             )} 
