ad191d4633d3984e708de0e43135cce94173e2cc
xy <xxinyitoh@gmail.com>
2025-06-08 16:03:42 +0800

    fix: edit path

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import {Moon, Sun} from "lucide-react";
 import {useEffect, useState} from "react";
-import {cn} from "@/lib/utils";
+import {cn} from "../lib/utils";
 
 
 export const ThemeToggle = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
     darkMode: 'class',
     content: [
       "./src/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
-      "./pages/**/*.{js,jsx,ts,tsx}",
-      "./app/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
       extend: {},
